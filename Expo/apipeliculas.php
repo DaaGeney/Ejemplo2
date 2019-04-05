803d4a521021b49b19c18e5ec3baaf4e6a4511ad
DaaGeney <diegoassia@gmail.com>
2019-04-05 01:22:17 -0500

    Otro

--- Expo/apipeliculas.php
+++ Expo/apipeliculas.php
@@ -32,28 +32,7 @@ class ApiPeliculas{
         }
     }
 
-    function getById($id){
-        $pelicula = new Pelicula();
-        $peliculas = array();
-        $peliculas["items"] = array();
-
-        $res = $pelicula->obtenerPelicula($id);
-
-        if($res->rowCount() == 1){
-            $row = $res->fetch();
-        
-            $item=array(
-                "id" => $row['id'],
-                "nombre" => $row['nombre'],
-                "imagen" => $row['imagen'],
-            );
-            array_push($peliculas["items"], $item);
-            $this->printJSON($peliculas);
-        }else{
-            $this->error('No hay elementos');
-        }
-    }
-
+    
     function add($item){
         $pelicula = new Pelicula();
 
